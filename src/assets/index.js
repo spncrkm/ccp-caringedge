c0d13c58b55b79259f803a2353230cea50dca3f6
Spencer Kim <spncrkm@gmail.com>
2024-09-20 15:05:55 -0400

    3d image carousel

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,31 @@
-import ccpLogo from './CCP-Logo_Color-R.webp'
-import heroimg from './juliane-liebermann-O-RKu3Aqnsw-unsplash cropped.jpg'
+import ccpLogo from "./CCP-Logo_Color-R.webp";
+import heroimg from "./juliane-liebermann-O-RKu3Aqnsw-unsplash cropped.jpg";
+import image1 from "./image1.jpg";
+import image2 from "./image2.jpg";
+import image3 from "./image3.jpg";
+import image4 from "./image4.jpg";
+import image5 from "./image5.jpg";
+import image6 from "./image6.jpg";
+import image7 from "./image7.jpg";
+import image8 from "./image8.jpg";
+import image9 from "./image9.jpg";
+import image10 from "./image10.jpg";
+import image11 from "./image11.jpg";
+import image12 from "./image12.jpg";
 
-
-
-export { ccpLogo, heroimg }
+export {
+  ccpLogo,
+  heroimg,
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
+  image12,
+};
--- src/components/landingPage/Features.jsx
+++ src/components/landingPage/Features.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { 
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10
+ } from '../../assets'
+
+
+const images = [
+  image1, image2, image3, image4, image5,
+  image6, image7, image8, image9, image10
+];
+
 
 const Features = () => {
   return (
-    <div>
-      Features
+    <div className='banner'>
+      <div className="slider" style={{ '--quantity': images.length }}>
+        {images.map((image,index) => (
+          <div key={index} className='item' style={{ '--position': index + 1}}>
+            <img src={image} alt={`Feature ${index + 1}`} />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
