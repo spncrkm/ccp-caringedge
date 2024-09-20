e9c22e5f69e1568f27bb5a169414a083721bdda1
Spencer Kim <spncrkm@gmail.com>
2024-09-20 15:15:26 -0400

    compressed images

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,17 +1,17 @@
 import ccpLogo from "./CCP-Logo_Color-R.webp";
 import heroimg from "./juliane-liebermann-O-RKu3Aqnsw-unsplash cropped.jpg";
-import image1 from "./image1.jpg";
-import image2 from "./image2.jpg";
-import image3 from "./image3.jpg";
-import image4 from "./image4.jpg";
-import image5 from "./image5.jpg";
-import image6 from "./image6.jpg";
-import image7 from "./image7.jpg";
-import image8 from "./image8.jpg";
-import image9 from "./image9.jpg";
-import image10 from "./image10.jpg";
-import image11 from "./image11.jpg";
-import image12 from "./image12.jpg";
+import image1 from "./image1 (1).jpg";
+import image2 from "./image2 (1).jpg";
+import image3 from "./image3 (1).jpg";
+import image4 from "./image4 (1).jpg";
+import image5 from "./image5 (1).jpg";
+import image6 from "./image6 (1).jpg";
+import image7 from "./image7 (1).jpg";
+import image8 from "./image8 (1).jpg";
+import image9 from "./image9 (1).jpg";
+import image10 from "./image10 (1).jpg";
+import image11 from "./image11 (1).jpg";
+import image12 from "./image12 (1).jpg";
 
 export {
   ccpLogo,
